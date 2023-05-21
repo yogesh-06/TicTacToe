9cc3b278396cb07c9774aef4a8ee206baccf1fc1
yogesh-06 <yograjput6662@gmail.com>
2023-05-21 16:59:27 +0530

    Hot fix

--- script.js
+++ script.js
@@ -1,58 +1,89 @@
-console.log("welcome to tic tac toe");
-let music = new Audio("music.mp3");
-let audioTurn = new Audio("ting.mp3");
-let overTheHorizon = new Audio("gameover.mp3");
-let gameOver = false;
-let turn = "X";
+const X_CLASS = 'x'
+const CIRCLE_CLASS = 'circle'
+const WINNING_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+const cellElements = document.querySelectorAll('[data-cell]')
+const board = document.getElementById('board')
+const winningMessageElement = document.getElementById('winningMessage')
+const restartButton = document.getElementById('restartButton')
+const winningMessageTextElement = document.querySelector('[data-winning-message-text]')
+let circleTurn
 
-const changeTurn = () => {
-  return turn === "X" ? "O" : "X";
-};
+startGame()
 
-const checkWin = () => {
-  let boxText = document.getElementsByClassName("boxText");
+restartButton.addEventListener('click', startGame)
 
-  let wins = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
+function startGame() {
+  circleTurn = false
+  cellElements.forEach(cell => {
+    cell.classList.remove(X_CLASS)
+    cell.classList.remove(CIRCLE_CLASS)
+    cell.removeEventListener('click', handleClick)
+    cell.addEventListener('click', handleClick, { once: true })
+  })
+  setBoardHoverClass()
+  winningMessageElement.classList.remove('show')
+}
 
-  wins.forEach((e) => {
-    if (
-      boxText[e[0]].innerText === boxText[e[1]].innerText &&
-      boxText[e[2]].innerText === boxText[e[1]].innerText &&
-      boxText[e[0]].innerText !== ""
-    ) {
-      document.querySelector(".info").innerText =
-        boxText[e[0]].innerText + "Won";
-      gameOver = true;
-      document
-        .querySelector(".imageBox")
-        .getElementsByTagName("img")[0].style.width = "200px";
-    }
-  });
-};
+function handleClick(e) {
+  const cell = e.target
+  const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
+  placeMark(cell, currentClass)
+  if (checkWin(currentClass)) {
+    endGame(false)
+  } else if (isDraw()) {
+    endGame(true)
+  } else {
+    swapTurns()
+    setBoardHoverClass()
+  }
+}
 
-let boxes = document.getElementsByClassName("box");
+function endGame(draw) {
+  if (draw) {
+    winningMessageTextElement.innerText = 'Draw!'
+  } else {
+    winningMessageTextElement.innerText = `${circleTurn ? "O's" : "X's"} Wins!`
+  }
+  winningMessageElement.classList.add('show')
+}
 
-Array.from(boxes).forEach((element) => {
-  let boxText = element.querySelector(".boxText");
-  element.addEventListener("click", () => {
-    if (boxText.innerText === "") {
-      boxText.innerText = turn;
-      turn = changeTurn();
-      // audioTurn.play();
-      checkWin();
-      if (!gameOver) {
-        document.getElementsByClassName("info")[0].innerText =
-          "Turn For" + turn;
-      }
-    }
-  });
-});
+function isDraw() {
+  return [...cellElements].every(cell => {
+    return cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS)
+  })
+}
+
+function placeMark(cell, currentClass) {
+  cell.classList.add(currentClass)
+}
+
+function swapTurns() {
+  circleTurn = !circleTurn
+}
+
+function setBoardHoverClass() {
+  board.classList.remove(X_CLASS)
+  board.classList.remove(CIRCLE_CLASS)
+  if (circleTurn) {
+    board.classList.add(CIRCLE_CLASS)
+  } else {
+    board.classList.add(X_CLASS)
+  }
+}
+
+function checkWin(currentClass) {
+  return WINNING_COMBINATIONS.some(combination => {
+    return combination.every(index => {
+      return cellElements[index].classList.contains(currentClass)
+    })
+  })
+}
